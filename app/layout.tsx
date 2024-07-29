7b6deee9ce8ddeadbdfecc70e5b39ccf8cadd186
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-07-29 12:39:37 +0500

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import clsx from 'clsx';
+import { inter } from './ui/fonts';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
